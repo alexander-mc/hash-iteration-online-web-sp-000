7e1249acb06997497e6c7bbe6859e7b48c631582
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-21 18:52:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   age = ""
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"if age <= 12
+    age == 12 ? "Happy Birthday #{kids_name}! You are now #{age} years old!": 
+    
   end
 end
